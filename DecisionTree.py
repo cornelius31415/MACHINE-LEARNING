969c6b650f906fb429b60475df4fde9c9118a985
cornelius31415 <50702246+cornelius31415@users.noreply.github.com>
2024-05-08 08:56:04 +0200

    Add files via upload

---
+++ DecisionTree.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 08:44:17 2024
+
+@author: cornelius
+"""
+
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+
+
+# ------------------------- DATA PREPARATION --------------------------
+
+df = pd.read_csv("gender_classification.csv")
+pd.set_option('display.max_columns', None)
+print(df)
+
+X = df.drop(columns=['gender'])
+y = df['gender']
+
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+
+
+# ------------------------- DECISION TREE -----------------------------
+
+decision_tree = DecisionTreeClassifier()
+decision_tree.fit(X_train, y_train)
+
+prediction = decision_tree.predict(X_test)
+accuracy = accuracy_score(y_test, prediction)
+
+print("The accuracy of the decision tree is: ", accuracy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
