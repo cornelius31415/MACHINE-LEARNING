73dd600759a5232eeaa327f09f5df8ea4db64c98
cornelius31415 <50702246+cornelius31415@users.noreply.github.com>
2024-05-08 10:16:16 +0200

    LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -8,19 +8,23 @@ Created on Wed May  8 08:57:39 2024
 
 # ---------------------------- IMPORTS ---------------------------------
 
+from sklearn.linear_model import LogisticRegression
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-
-
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
 
 # ------------------------- DATA PREPARATION --------------------------
 
 df = pd.read_csv("gender_classification.csv")
 pd.set_option('display.max_columns', None)
 print(df)
+
+label_encoder = LabelEncoder()
+df["gender"] = label_encoder.fit_transform(df["gender"])
 
 X = df.drop(columns=['gender'])
 y = df['gender']
@@ -35,6 +39,64 @@ log_regression = LogisticRegression()
 log_regression.fit(X_train,y_train)
 
 prediction = log_regression.predict(X_test)
-accuracy = accuracy_score(y_test, prediction)
 
-print("The Logistic Regression Accuracy is: ", accuracy)
+
+
+# ------------------------- CONFUSION MATRIX --------------------------
+
+"""
+                          Interesting  Metrics
+            
+                            TP: True Positive
+                            TN: True Negative
+                            FP: False Positive
+                            FN: False Negative
+
+                
+            1. Accuracy    =   (TP + TN) / (Total Predictions)
+            How often the model is correct
+            
+            2. Precision   =      TP / (TP + FP)
+            What percentage of the positives predicted is truly positive
+            
+            3. Sensitivity =      TP / (TP + FN)   (also called Recall)
+            What percentage of the positives is actually predicted positive
+            
+            4. F1 Score    =   2 * ((Precision * Sensitivity) / (Precision + Sensitivity))
+            Harmonic Mean of Precision and Sensitivity
+            Values between 1 and 0. F1 = 1 indicates perfect precision and recall (sensitivity)
+                                
+
+
+"""
+
+
+conf_matrix = confusion_matrix(y_test, prediction)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = conf_matrix, 
+                                            display_labels = [0,1])
+cm_display.plot()
+plt.show()
+
+
+accuracy = metrics.accuracy_score(y_test, prediction)
+precision = metrics.precision_score(y_test, prediction)
+sensitivity = metrics.recall_score(y_test, prediction)
+f1_score = metrics.f1_score(y_test, prediction)
+
+print("The accuracy of the decision tree is: ", accuracy)
+print("The precision of the decision tree is: ", precision)
+print("The sensitivity of the decision tree is: ", sensitivity)
+print("The F1 Score of the decision tree is: ", f1_score)
+
+
+
+
+
+
+
+
+
+
+
+
+
