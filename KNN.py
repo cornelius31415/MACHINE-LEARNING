d33320c677b8355032c60dd334fb5fa70572a6cb
cornelius31415 <50702246+cornelius31415@users.noreply.github.com>
2024-05-08 10:17:55 +0200

    KNN.py

--- KNN.py
+++ KNN.py
@@ -8,11 +8,14 @@ Created on Wed May  8 09:09:11 2024
 
 # ---------------------------- IMPORTS ---------------------------------
 
+from sklearn.neighbors import KNeighborsClassifier
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
 
 
 
@@ -21,6 +24,9 @@ from sklearn.neighbors import KNeighborsClassifier
 df = pd.read_csv("gender_classification.csv")
 pd.set_option('display.max_columns', None)
 print(df)
+
+label_encoder = LabelEncoder()
+df["gender"] = label_encoder.fit_transform(df["gender"])
 
 X = df.drop(columns=['gender'])
 y = df['gender']
@@ -35,6 +41,63 @@ knn = KNeighborsClassifier()
 knn.fit(X_train, y_train)
 
 prediction = knn.predict(X_test)
-accuracy = accuracy_score(y_test, prediction)
 
-print("The KNN Accuracy is: ", accuracy)
+
+# ------------------------- CONFUSION MATRIX --------------------------
+
+"""
+                          Interesting  Metrics
+            
+                            TP: True Positive
+                            TN: True Negative
+                            FP: False Positive
+                            FN: False Negative
+
+                
+            1. Accuracy    =   (TP + TN) / (Total Predictions)
+            How often the model is correct
+            
+            2. Precision   =      TP / (TP + FP)
+            What percentage of the positives predicted is truly positive
+            
+            3. Sensitivity =      TP / (TP + FN)   (also called Recall)
+            What percentage of the positives is actually predicted positive
+            
+            4. F1 Score    =   2 * ((Precision * Sensitivity) / (Precision + Sensitivity))
+            Harmonic Mean of Precision and Sensitivity
+            Values between 1 and 0. F1 = 1 indicates perfect precision and recall (sensitivity)
+                                
+
+
+"""
+
+
+conf_matrix = confusion_matrix(y_test, prediction)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = conf_matrix, 
+                                            display_labels = [0,1])
+cm_display.plot()
+plt.show()
+
+
+accuracy = metrics.accuracy_score(y_test, prediction)
+precision = metrics.precision_score(y_test, prediction)
+sensitivity = metrics.recall_score(y_test, prediction)
+f1_score = metrics.f1_score(y_test, prediction)
+
+print("The accuracy of the decision tree is: ", accuracy)
+print("The precision of the decision tree is: ", precision)
+print("The sensitivity of the decision tree is: ", sensitivity)
+print("The F1 Score of the decision tree is: ", f1_score)
+
+
+
+
+
+
+
+
+
+
+
+
+
