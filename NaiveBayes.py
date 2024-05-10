90a80db34d6d0553f54d89a5ec67538154ab11f3
cornelius31415 <50702246+cornelius31415@users.noreply.github.com>
2024-05-10 12:45:48 +0200

    Add files via upload

---
+++ NaiveBayes.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 10 12:43:30 2024
+
+@author: cornelius
+"""
+
+
+
+# ---------------------------- IMPORTS ---------------------------------
+from sklearn.naive_bayes import GaussianNB
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+
+
+
+# ------------------------- DATA PREPARATION --------------------------
+
+df = pd.read_csv("gender_classification.csv")
+pd.set_option('display.max_columns', None)
+
+
+label_encoder = LabelEncoder()
+df["gender"] = label_encoder.fit_transform(df["gender"])
+
+features = df.drop(columns=['gender'])
+labels = df['gender']
+
+
+
+feature_train, feature_test, label_train, label_test = train_test_split(
+    features,labels, test_size=0.2, random_state=42)
+
+
+
+
+# --------------------    Naives Bayes   -------------------------------
+
+naive = GaussianNB()
+naive.fit(feature_train,label_train)
+prediction = naive.predict(feature_test)
+
+
+
+accuracy = metrics.accuracy_score(label_test, prediction)
+precision = metrics.precision_score(label_test, prediction)
+sensitivity = metrics.recall_score(label_test, prediction)
+f1_score = metrics.f1_score(label_test, prediction)
+
+"""
+1. Accuracy:        What percentage of shrooms has been correctly identified
+2. Precision:       What percentage of predicted poisonous shrooms is actually poisonous
+3. Recall:          What percentage of the poisonous shrooms is correctly identified as poisonous.
+4. F1 Score:        The balance between precision and recall
+"""
+
+print("The accuracy of the decision tree is: ", accuracy)
+print("The precision of the decision tree is: ", precision)
+print("The sensitivity of the decision tree is: ", sensitivity)
+print("The F1 Score of the decision tree is: ", f1_score)
